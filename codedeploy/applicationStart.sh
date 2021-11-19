3c23326fc84205ea51804a19d7b8bf421a92d8fe
Naveen Kumar Buddhala <buddhala.n@northeastern.edu>
2021-11-19 12:48:54 -0500

    demo

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,5 @@
 sudo chmod 777 /home/ubuntu/spring-mvc-rest-0.0.1-SNAPSHOT.jar
-sudo chmod 777 -R /home/ubuntu/*
+sudo chmod 777 -R .
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo systemctl stop tomcat9
 sudo systemctl start amazon-cloudwatch-agent
