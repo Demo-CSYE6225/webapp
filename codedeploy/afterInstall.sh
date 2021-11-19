ee1ef67b0e4bd3af264d0ae84c847682f1316308
Naveen Kumar Buddhala <buddhala.n@northeastern.edu>
2021-11-19 12:18:29 -0500

    demo

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,3 +5,4 @@ sudo chmod 777 amazon-cloudwatch-agent
 sudo systemctl start amazon-cloudwatch-agent
 
 sudo chmod 777 /home/ubuntu/spring-mvc-rest-0.0.1-SNAPSHOT.jar
+sudo java -jar /home/ubuntu/spring-mvc-rest-0.0.1-SNAPSHOT.jar stop
