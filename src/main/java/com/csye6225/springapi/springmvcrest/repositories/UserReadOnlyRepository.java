67022a6541485b40a8ad3987d4731dcd9a9d11e5
Naveen Kumar Buddhala <buddhala.n@northeastern.edu>
2021-12-03 15:04:31 -0500

    test

--- src/main/java/com/csye6225/springapi/springmvcrest/repositories/UserReadOnlyRepository.java
+++ src/main/java/com/csye6225/springapi/springmvcrest/repositories/UserReadOnlyRepository.java
@@ -1,9 +1,12 @@
 package com.csye6225.springapi.springmvcrest.repositories;
 import com.csye6225.springapi.springmvcrest.Configuration.ReadOnlyRepository;
 import com.csye6225.springapi.springmvcrest.domain.User;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.UUID;
 
 @ReadOnlyRepository
-public interface UserReadOnlyRepository extends Repository {
+public interface UserReadOnlyRepository extends JpaRepository<User, UUID> {
     User findByUsername(String username);
 }
