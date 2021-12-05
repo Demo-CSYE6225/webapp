28eff65adf29dffbb0c19e32c7c26cdefeb42960
Naveen Kumar Buddhala <buddhala.n@northeastern.edu>
2021-12-04 20:50:03 -0500

    test

--- src/main/java/com/csye6225/springapi/springmvcrest/Configuration/ReadOnlyEntityManagerConfiguration.java
+++ src/main/java/com/csye6225/springapi/springmvcrest/Configuration/ReadOnlyEntityManagerConfiguration.java
@@ -1,49 +1,49 @@
-package com.csye6225.springapi.springmvcrest.Configuration;
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = "com.csye6225.springapi.springmvcrest",
-        includeFilters= @ComponentScan.Filter(ReadOnlyRepository.class),
-        entityManagerFactoryRef = "readEntityManagerFactory"
-)
-public class ReadOnlyEntityManagerConfiguration {
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.readUrl}")
-    private String readUrl;
-
-    @Bean
-    public DataSource readDataSource() throws Exception {
-        return DataSourceBuilder.create()
-                .url(readUrl)
-                .username(username)
-                .password(password)
-                .build();
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean readEntityManagerFactory(
-            EntityManagerFactoryBuilder builder,
-            @Qualifier("readDataSource") DataSource dataSource) {
-        return builder.dataSource(dataSource)
-                .packages("com.csye6225.springapi.springmvcrest")
-                .persistenceUnit("read")
-                .build();
-    }
-}
+//package com.csye6225.springapi.springmvcrest.Configuration;
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "com.csye6225.springapi.springmvcrest",
+//        includeFilters= @ComponentScan.Filter(ReadOnlyRepository.class),
+//        entityManagerFactoryRef = "readEntityManagerFactory"
+//)
+//public class ReadOnlyEntityManagerConfiguration {
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Value("${spring.datasource.readUrl}")
+//    private String readUrl;
+//
+//    @Bean
+//    public DataSource readDataSource() throws Exception {
+//        return DataSourceBuilder.create()
+//                .url(readUrl)
+//                .username(username)
+//                .password(password)
+//                .build();
+//    }
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean readEntityManagerFactory(
+//            EntityManagerFactoryBuilder builder,
+//            @Qualifier("readDataSource") DataSource dataSource) {
+//        return builder.dataSource(dataSource)
+//                .packages("com.csye6225.springapi.springmvcrest")
+//                .persistenceUnit("read")
+//                .build();
+//    }
+//}
